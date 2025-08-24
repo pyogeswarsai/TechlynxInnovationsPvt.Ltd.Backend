6b10f39ff5a004444c10847096a9052340d165eb
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-08-24 20:08:31 +0530

    final commit changed access to CORS

--- src/main/java/com/example/demo/config/CorsConfig.java
+++ src/main/java/com/example/demo/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173") // frontend origin
+                        .allowedOrigins("https://techlynxinnovationspvtltd.netlify.app") // frontend origin
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
