6cbcf98752d855d4295ca9c298d6038874dbf6a1
pyogeswarsai <pyogeswarsai1249@gmail.com>
2025-08-25 06:35:09 +0530

    changes in reset passsword

--- src/main/java/com/example/demo/service/PasswordResetService.java
+++ src/main/java/com/example/demo/service/PasswordResetService.java
@@ -21,6 +21,7 @@ public class PasswordResetService {
 
     @Autowired
     private JavaMailSender mailSender;
+    
 
     @Autowired
     public PasswordResetService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
@@ -47,7 +48,7 @@ public class PasswordResetService {
     }
 
     private void sendPasswordResetEmail(String toEmail, String token) {
-        String resetUrl = "https://techlynxinnovationspvtltd.netlify.app/api/auth/reset-password?token=" + token; 
+        String resetUrl = "https://techlynxinnovationspvtltd.netlify.app/reset-password?token=" + token; 
         // change this to your frontend URL
 
         SimpleMailMessage message = new SimpleMailMessage();
